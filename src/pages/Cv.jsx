9037f30740dbeb2dad911fd673b777c5e7a77a84
Elhef_2038 <lfougerard@gmail.com>
2025-05-01 08:17:47 +0200

    Mise en page page CV (section FACULTÉ - licence Philosophie)

--- src/pages/Cv.jsx
+++ src/pages/Cv.jsx
@@ -69,8 +69,74 @@ function Cv() {
                 </div>
 
                 <div className='university'>
-                    <h2>MON PARCOURS UNIVERSITAIRE</h2>
-                    <p className='formation'>Licence de Philosophie</p>
+                    <div className='university-title'>
+                        <h2>MON PARCOURS UNIVERSITAIRE</h2>
+                        <p className='formation'>Licence de Philosophie</p>
+                    </div>
+
+                    <div className='university-content'>
+                        <h3 className='first-year'>2021-2022 : Première année d'Hypokhâgne<br /> (en école préparatoire)</h3>
+                        <ul>
+                            <li>════════════════════► Histoire</li>
+                            <li>════════════════════► Géographie</li>
+                            <li>════════════════════► Français</li>
+                            <li>════════════════════► Littérature</li>
+                            <li>════════════════════► Latin (en LV2)</li>
+                            <li>════════════════════► Anglais et Espagnol (en LV3)</li>
+                        </ul>
+
+                        <h3 className='second-third-year'>2022-2023 : Deuxième année de licence Philosophie (en faculté)</h3>
+                        <p className='semestry'>SEMESTRE 1</p>
+                        <ul>
+                            <li>════════════════════► Esthétique</li>
+                            <li>════════════════════► Métaphysique</li>
+                            <li>════════════════════► Philosophie contemporaine</li>
+                            <li>════════════════════► Philosophie des sciences et épistémologiue</li>
+                            <li>════════════════════► Philosophie moderne</li>
+                            <li>════════════════════► Philosophie morale et politique</li>
+                            <li>════════════════════► Latin (en option)</li>
+                            <li>════════════════════► Anglais (en LV2) et Espagnol (en LV3)</li>
+                        </ul>
+
+                        <p className='semestry'>SEMESTRE 2</p>
+                        <ul>
+                            <li>════════════════════► Philosophie du droit</li>
+                            <li>════════════════════► Métaphysique</li>
+                            <li>════════════════════► Philosophie des sciences humaines et sociales</li>
+                            <li>════════════════════► Philosophie des sciences et épistémologiue</li>
+                            <li>════════════════════► Philosophie moderne</li>
+                            <li>════════════════════► Logique</li>
+                            <li>════════════════════► Latin (en option)</li>
+                            <li>════════════════════► Anglais (en LV2) et Espagnol (en LV3)</li>
+                        </ul>
+
+                        <h3 className='second-third-year'>2023-2024 : Troisième année de licence Philosophie (en faculté)</h3>
+                        <p className='semestry'>SEMESTRE 1</p>
+                        <ul>
+                            <li>════════════════════► Logique</li>
+                            <li>════════════════════► Métaphysique</li>
+                            <li>════════════════════► Philosophie moderne et contemporaine</li>
+                            <li>════════════════════► Philosophie des sciences et épistémologiue</li>
+                            <li>════════════════════► Philosophie morale et politique</li>
+                            <li>════════════════════► Philosophie ancienn</li>
+                            <li>════════════════════► Latin (en option)</li>
+                            <li>════════════════════► Anglais (en LV2) et Espagnol (en LV3)</li>
+                        </ul>
+
+                        <p className='semestry'>SEMESTRE 2</p>
+                        <ul>
+                            <li>════════════════════► Logique</li>
+                            <li>════════════════════► Métaphysique</li>
+                            <li>════════════════════► Philosophie ancienne et médiévale</li>
+                            <li>════════════════════► Philosophie contemporaine</li>
+                            <li>════════════════════► Philosophie moderne et contemporaine</li>
+                            <li>════════════════════► Philosophie de l'art</li>
+                            <li>════════════════════► Philosophie des sciences et épistémologiue</li>
+                            <li>════════════════════► Philosophie politique</li>
+                            <li>════════════════════► Latin (en option)</li>
+                            <li>════════════════════► Anglais (en LV2) et Espagnol (en LV3)</li>
+                        </ul>
+                    </div>
                 </div>
             </section>
 
