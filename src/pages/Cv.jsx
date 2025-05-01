9ee49cd83473a79ab0e6977548b40928017107fe
Elhef_2038 <lfougerard@gmail.com>
2025-05-01 17:41:34 +0200

    Ajout des couleurs sur la page CV

--- src/pages/Cv.jsx
+++ src/pages/Cv.jsx
@@ -4,6 +4,7 @@ import { CertificationCard } from '../components/components.jsx';
 import { TitleCurriculum } from '../components/components.jsx';
 import projectsDatas from '../assets/projects.json';
 import certifications from '../assets/certifications.json';
+import backgroundImageDark from '../images/background-image-cv-dark.png';
 
 import { useState } from 'react';
 
@@ -14,20 +15,14 @@ function Cv() {
 
     return (
         <div className='cv-page'>
-            <section className='part-cv-1'>
-                <div className='title-cv-container'>
-                    <div className='cv-title'>
+            <section className="part-cv-1">
+                <div className="title-cv-container">
+                    <div className="cv-title">
                         <TitleCurriculum />
                         <a href="#" download>Télécharger</a>
                     </div>
-                    <div className="cv-image">
-                        <img
-                            alt="cv"
-                            src={cvImage}
-                        />
-                    </div>
                 </div>
-            </section >
+            </section>
 
             <section className='part-cv-2'>
                 <div className='openclassrooms'>
@@ -77,64 +72,64 @@ function Cv() {
                     <div className='university-content'>
                         <h3 className='first-year'>2021-2022 : Première année d'Hypokhâgne<br /> (en école préparatoire)</h3>
                         <ul>
-                            <li>════════════════════► Histoire</li>
-                            <li>════════════════════► Géographie</li>
-                            <li>════════════════════► Français</li>
-                            <li>════════════════════► Littérature</li>
-                            <li>════════════════════► Latin (en LV2)</li>
-                            <li>════════════════════► Anglais et Espagnol (en LV3)</li>
+                            <li className='courses-university'>════════════════════► Histoire</li>
+                            <li className='courses-university'>════════════════════► Géographie</li>
+                            <li className='courses-university'>════════════════════► Français</li>
+                            <li className='courses-university'>════════════════════► Littérature</li>
+                            <li className='courses-university'>════════════════════► Latin (en LV2)</li>
+                            <li className='courses-university'>════════════════════► Anglais et Espagnol (en LV3)</li>
                         </ul>
 
                         <h3 className='second-third-year'>2022-2023 : Deuxième année de licence Philosophie (en faculté)</h3>
                         <p className='semestry'>SEMESTRE 1</p>
                         <ul>
-                            <li>════════════════════► Esthétique</li>
-                            <li>════════════════════► Métaphysique</li>
-                            <li>════════════════════► Philosophie contemporaine</li>
-                            <li>════════════════════► Philosophie des sciences et épistémologiue</li>
-                            <li>════════════════════► Philosophie moderne</li>
-                            <li>════════════════════► Philosophie morale et politique</li>
-                            <li>════════════════════► Latin (en option)</li>
-                            <li>════════════════════► Anglais (en LV2) et Espagnol (en LV3)</li>
+                            <li className='courses-university'>════════════════════► Esthétique</li>
+                            <li className='courses-university'>════════════════════► Métaphysique</li>
+                            <li className='courses-university'>════════════════════► Philosophie contemporaine</li>
+                            <li className='courses-university'>════════════════════► Philosophie des sciences et épistémologiue</li>
+                            <li className='courses-university'>════════════════════► Philosophie moderne</li>
+                            <li className='courses-university'>════════════════════► Philosophie morale et politique</li>
+                            <li className='courses-university'>════════════════════► Latin (en option)</li>
+                            <li className='courses-university'>════════════════════► Anglais (en LV2) et Espagnol (en LV3)</li>
                         </ul>
 
                         <p className='semestry'>SEMESTRE 2</p>
                         <ul>
-                            <li>════════════════════► Philosophie du droit</li>
-                            <li>════════════════════► Métaphysique</li>
-                            <li>════════════════════► Philosophie des sciences humaines et sociales</li>
-                            <li>════════════════════► Philosophie des sciences et épistémologiue</li>
-                            <li>════════════════════► Philosophie moderne</li>
-                            <li>════════════════════► Logique</li>
-                            <li>════════════════════► Latin (en option)</li>
-                            <li>════════════════════► Anglais (en LV2) et Espagnol (en LV3)</li>
+                            <li className='courses-university'>═══════════════════► Philosophie du droit</li>
+                            <li className='courses-university'>════════════════════► Métaphysique</li>
+                            <li className='courses-university'>════════════════════► Philosophie des sciences humaines et sociales</li>
+                            <li className='courses-university'>════════════════════► Philosophie des sciences et épistémologiue</li>
+                            <li className='courses-university'>════════════════════► Philosophie moderne</li>
+                            <li className='courses-university'>════════════════════► Logique</li>
+                            <li className='courses-university'>════════════════════► Latin (en option)</li>
+                            <li className='courses-university'>════════════════════► Anglais (en LV2) et Espagnol (en LV3)</li>
                         </ul>
 
                         <h3 className='second-third-year'>2023-2024 : Troisième année de licence Philosophie (en faculté)</h3>
                         <p className='semestry'>SEMESTRE 1</p>
                         <ul>
-                            <li>════════════════════► Logique</li>
-                            <li>════════════════════► Métaphysique</li>
-                            <li>════════════════════► Philosophie moderne et contemporaine</li>
-                            <li>════════════════════► Philosophie des sciences et épistémologiue</li>
-                            <li>════════════════════► Philosophie morale et politique</li>
-                            <li>════════════════════► Philosophie ancienn</li>
-                            <li>════════════════════► Latin (en option)</li>
-                            <li>════════════════════► Anglais (en LV2) et Espagnol (en LV3)</li>
+                            <li className='courses-university'>════════════════════► Logique</li>
+                            <li className='courses-university'>════════════════════► Métaphysique</li>
+                            <li className='courses-university'>════════════════════► Philosophie moderne et contemporaine</li>
+                            <li className='courses-university'>════════════════════► Philosophie des sciences et épistémologiue</li>
+                            <li className='courses-university'>════════════════════► Philosophie morale et politique</li>
+                            <li className='courses-university'>════════════════════► Philosophie ancienn</li>
+                            <li className='courses-university'>════════════════════► Latin (en option)</li>
+                            <li className='courses-university'>════════════════════► Anglais (en LV2) et Espagnol (en LV3)</li>
                         </ul>
 
                         <p className='semestry'>SEMESTRE 2</p>
                         <ul>
-                            <li>════════════════════► Logique</li>
-                            <li>════════════════════► Métaphysique</li>
-                            <li>════════════════════► Philosophie ancienne et médiévale</li>
-                            <li>════════════════════► Philosophie contemporaine</li>
-                            <li>════════════════════► Philosophie moderne et contemporaine</li>
-                            <li>════════════════════► Philosophie de l'art</li>
-                            <li>════════════════════► Philosophie des sciences et épistémologiue</li>
-                            <li>════════════════════► Philosophie politique</li>
-                            <li>════════════════════► Latin (en option)</li>
-                            <li>════════════════════► Anglais (en LV2) et Espagnol (en LV3)</li>
+                            <li className='courses-university'>════════════════════► Logique</li>
+                            <li className='courses-university'>════════════════════► Métaphysique</li>
+                            <li className='courses-university'>════════════════════► Philosophie ancienne et médiévale</li>
+                            <li className='courses-university'>════════════════════► Philosophie contemporaine</li>
+                            <li className='courses-university'>════════════════════► Philosophie moderne et contemporaine</li>
+                            <li className='courses-university'>════════════════════► Philosophie de l'art</li>
+                            <li className='courses-university'>════════════════════► Philosophie des sciences et épistémologiue</li>
+                            <li className='courses-university'>════════════════════► Philosophie politique</li>
+                            <li className='courses-university'>════════════════════► Latin (en option)</li>
+                            <li className='courses-university'>════════════════════► Anglais (en LV2) et Espagnol (en LV3)</li>
                         </ul>
                     </div>
                 </div>
