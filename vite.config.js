b84c6bd991b633285944915159c1ef819c5e4a9a
Elhef_2038 <lfougerard@gmail.com>
2025-05-09 13:21:58 +0200

    Correction de l'URL de déploiement

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Important pour GitHub Pages
 export default defineConfig({
+  base: '/PORTFOLIO/',
   plugins: [react()],
 })
